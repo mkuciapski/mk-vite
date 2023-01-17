72f07217d81e0023a29535a888a538ac63ed2cee
mkuciapski <m.kuciapski@gmail.com>
2023-01-17 14:18:56 +0100

    mk vite

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/*eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js, ts, vue}", "./src/**/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
